1c41cb4a6afc922bf395993c517a81e109ebad0d
Zhiyu Zhang <zhiyuzhang1999@163.com>
2022-08-27 16:02:28 +0800

    change single quote to double

--- 1_🏠_Scholar_Watcher.py
+++ 1_🏠_Scholar_Watcher.py
@@ -8,21 +8,21 @@ from math import ceil
 
 
 conf = ConfigParser()
-conf.read("config.ini", encoding='utf-8')
-Proxy = conf['Proxy']
-Authors = conf['Authors']
-Settings = conf['Settings']
+conf.read("config.ini", encoding="utf-8")
+Proxy = conf["Proxy"]
+Authors = conf["Authors"]
+Settings = conf["Settings"]
 
 # need to add server check here
-if Settings['mode'] == 'local':
-    os.environ["http_proxy"] = Proxy['http_proxy']
-    os.environ["https_proxy"] = conf['Proxy']['https_proxy']
+if Settings["mode"] == "local":
+    os.environ["http_proxy"] = Proxy["http_proxy"]
+    os.environ["https_proxy"] = conf["Proxy"]["https_proxy"]
 
 searcher = SearchEngine()
-citation = Citation(Settings['db_path'])
+citation = Citation(Settings["db_path"])
 
 # auto update at 00:00:01 everyday
-initial_timer = threading.Timer(getSecondsToTime(0, 0, 1), autoUpdateEveryDay, (searcher, citation, conf))
+initial_timer = threading.Timer(getSecondsToTime(8, 5, 1), autoUpdateEveryDay, (searcher, citation, conf))
 initial_timer.setDaemon(True)
 initial_timer.start()
 
@@ -36,69 +36,69 @@ df_present = pd.DataFrame(d_citation).T
 st.set_page_config(
      page_title="Scholar Watcher 2022@Zhiyu Zhang",
      page_icon="🧊",
-     layout=Settings['layout'],
+     layout=Settings["layout"],
      initial_sidebar_state="expanded",
      menu_items={
-         'Get Help': 'https://www.extremelycoolapp.com/help',
-         'Report a bug': "https://www.extremelycoolapp.com/bug",
-         'About': "## Scholar Watcher\n ## 2022@Zhiyu Zhang"
+         "Get Help": "https://www.extremelycoolapp.com/help",
+         "Report a bug": "https://github.com/QGrain/Scholar-Watcher/issues",
+         "About": "## Scholar Watcher\n ## 2022@Zhiyu Zhang"
     }
 )
 
 # 🎈🧨✨🎉🎯🎖️
-st.title('Scholar Watcher 🏠')
-st.header('1 Google Scholar Citations')
+st.title("Scholar Watcher 🏠")
+st.header("1 Google Scholar Citations")
 
-cite_tab1, cite_tab2 = st.tabs(['DataFrame (Interactable)', 'Table (Static)'])
+cite_tab1, cite_tab2 = st.tabs(["DataFrame (Interactable)", "Table (Static)"])
 with cite_tab1:
-    st.info('💡 Tips: This DataFrame is **interactable**. You can **sort** the columns by clicking the column name and **resize** the columns by dragging the borders.')
+    st.info("💡 Tips: This DataFrame is **interactable**. You can **sort** the columns by clicking the column name and **resize** the columns by dragging the borders.")
     st.dataframe(df_present, height=385, width=500)
 
 with cite_tab2:
-    st.info('💡 Tips: This table is **static**.')
+    st.info("💡 Tips: This table is **static**.")
     st.table(df_present)
 
-st.warning('Citation table would update **once a day**. Force update would update **immediately** and **time-costly**.')
-btn_click = st.button('Force Update', help='This would update the citation immediately')
+st.warning("Citation table would update **once a day**. Force update would update **immediately** and **time-costly**.")
+btn_click = st.button("Force Update", help="This would update the citation immediately")
 
 
 if btn_click == True:
     progress_msg = st.empty()
     progress_bar = st.progress(0)  
     for i, author_label in enumerate(Authors):
-        progress_msg.text(f'Iterate {i+1}/{len(Authors)} authors')
+        progress_msg.text(f"Iterate {i+1}/{len(Authors)} authors")
         p = int(i/len(Authors) * 100)
         checkUpdate(searcher, citation, conf, single_author=author_label, force=True)
         progress_bar.progress(p)
     progress_bar.progress(100)
     btn_click = False
-    st.success('Force check citation update done!')
+    st.success("Force check citation update done!")
     st.balloons()
 
 
 # Add a selectbox to the sidebar:
 layout_selectbox = st.sidebar.selectbox(
-    'Page layout',
-    ('centered', 'wide')
+    "Page layout",
+    ("centered", "wide")
 )
 
 d = st.sidebar.date_input(
     "Select a date filter",
     date(2022, 8, 6))
-st.sidebar.write('You select:', d)
-st.sidebar.info('Well this ⬆ sidebar module is still in developing actually.')
+st.sidebar.write("You select:", d)
+st.sidebar.info("Well this ⬆ sidebar module is still in developing actually.")
 
 
 
-st.header('2 Focus Authors')
+st.header("2 Focus Authors")
 
 focus_authors = st.multiselect(
-     'Pleasec select the focus authors',
-     conf['Authors'],
-     ['kaichen', 'guozhumeng', 'xiaofengwang'],
-     help='Choose the focus authors, and display the analytics.')
-# st.text(f'You have selected {len(focus_authors)} authors to focus on.')
-st.info(f'You have selected **{len(focus_authors)}** authors to focus on.')
+     "Pleasec select the focus authors",
+     conf["Authors"],
+     ["kaichen", "guozhumeng", "xiaofengwang"],
+     help="Choose the focus authors, and display the analytics.")
+# st.text(f"You have selected {len(focus_authors)} authors to focus on.")
+st.info(f"You have selected **{len(focus_authors)}** authors to focus on.")
 
 focus_author_ids = []
 focus_chart_columns = []
@@ -106,13 +106,13 @@ for focus_author_label in focus_authors:
     focus_chart_columns.append(focus_author_label)
     focus_author_ids.append(Authors[focus_author_label])
 
-tab1, tab2, tab3, tab4 = st.tabs(['🚀 Metrix', '📈 Charts', '🔎 Recent Publications', '🎯 Todo'])
+tab1, tab2, tab3, tab4 = st.tabs(["🚀 Metrix", "📈 Charts", "🔎 Recent Publications", "🎯 Todo"])
 with tab1:
-    st.subheader('Citation Metrix 🚀')
+    st.subheader("Citation Metrix 🚀")
     if len(focus_authors) == 0:
-        st.warning('No focus author')
+        st.warning("No focus author")
     else:
-        if Settings['layout'] == 'wide':
+        if Settings["layout"] == "wide":
             max_col_num = 8
         else:
             max_col_num = 5
@@ -123,38 +123,38 @@ with tab1:
         for i in range(len(focus_authors)):
             row = int(i/max_col_num)
             col = i % max_col_num
-            author_name = d_all[Authors[focus_authors[i]]]['name']
-            matrix[row][col].metric(label=focus_authors[i], value=d_citation[author_name]['citation'], delta=d_citation[author_name]['increase'])
+            author_name = d_all[Authors[focus_authors[i]]]["name"]
+            matrix[row][col].metric(label=focus_authors[i], value=d_citation[author_name]["citation"], delta=d_citation[author_name]["increase"])
 
 with tab2:
-    st.subheader('Analysis Charts 📈')
+    st.subheader("Analysis Charts 📈")
     if len(focus_authors) == 0:
-        st.warning('No focus author')
+        st.warning("No focus author")
     else:
         # focus_plot_data, earliest_date = getPlotData(d_all, author_ids)
-        # plt.title('Focus Authors Citation Over Time')
+        # plt.title("Focus Authors Citation Over Time")
         # for i in range(len(focus_authors)):
-        #     fig = plt.plot(focus_plot_data[i]['X_DATA'], focus_plot_data[i]['Y_DATA'], label=focus_authors[i])
+        #     fig = plt.plot(focus_plot_data[i]["X_DATA"], focus_plot_data[i]["Y_DATA"], label=focus_authors[i])
         # plt.legend()
-        # plt.xlabel('Date')
-        # plt.ylabel('Citation')
+        # plt.xlabel("Date")
+        # plt.ylabel("Citation")
         # st.pyplot(fig)
 
         # print(author_ids)
         focus_chart_data = pd.DataFrame(np.array(citation.getSequence(focus_author_ids)).T, columns=focus_chart_columns)
-        # focus_chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+        # focus_chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
         # print(focus_chart_data)
 
         st.line_chart(focus_chart_data)
-        st.write('Todo...')
+        st.write("Todo...")
 
 with tab3:
-    st.subheader('Recent Publications 🔎')
-    st.warning('This is runtime fetching, so it will take some time. **Please wait after pressing Go...**')
+    st.subheader("Recent Publications 🔎")
+    st.warning("This is runtime fetching, so it will take some time. **Please wait after pressing Go...**")
     tab3_col1, tab3_col2 = st.columns(2)
-    latest_k = tab3_col1.number_input(label='Input Latest K (1~20) Publications to fetch', min_value=1, max_value=20, value=3, step=1, format='%d')
-    tab3_col2.caption('Click here ⬇')
-    latest_go = tab3_col2.button('GO!')
+    latest_k = tab3_col1.number_input(label="Input Latest K (1~20) Publications to fetch", min_value=1, max_value=20, value=3, step=1, format="%d")
+    tab3_col2.caption("Click here ⬇")
+    latest_go = tab3_col2.button("GO!")
     if latest_go == True:
         for i, author_id in enumerate(focus_author_ids):
             # recent_pubs = searcher.fetchRecentTopKPub(author_id, 3)
@@ -165,18 +165,18 @@ with tab3:
         latest_go = False
 
 with tab4:
-    st.subheader('Todo 🎯')
-    st.warning('Todo, you can raise issues to help improve this. (https://github.com/QGrain/Scholar-Watcher/issues)')
+    st.subheader("Todo 🎯")
+    st.warning("Todo, you can raise issues to help improve this. (https://github.com/QGrain/Scholar-Watcher/issues)")
 
 
-st.header('3 About This Site')
+st.header("3 About This Site")
 
-st.markdown('Hello, welcome to **Scholar Watcher**, which could watching the google scholar citation changes of the following authors.')
+st.markdown("Hello, welcome to **Scholar Watcher**, which could watching the google scholar citation changes of the following authors.")
 
 with st.expander("Easter Egg"):
     # st.snow()
     st.write("""
-        Emmm, I am supposed to put an Easter egg here. But I don't have anyting.  :p
+        Emmm, I am supposed to put an Easter egg here. But I don"t have anyting.  :p
         \nSo, what about following me on github (https://github.com/QGrain/)?
     """)
     st.image("https://avatars.githubusercontent.com/u/38586306?v=4")
--- scholar_watcher_api.py
+++ scholar_watcher_api.py
@@ -14,7 +14,7 @@ class SearchEngine():
         pass
 
     def search(self, val, method):
-        if method == 'id':
+        if method == "id":
             return self.searchByID(val)
 
     def searchByID(self, user_id):
@@ -33,11 +33,11 @@ class SearchEngine():
 
         ret_pubs = {}
         cnt = 0
-        for pub in author['publications']:
+        for pub in author["publications"]:
             if cnt >= top_k:
                 break
-            if 'pub_year' in pub['bib'] and str(pub['bib']['pub_year']) == this_year_str:
-                ret_pubs[pub['bib']['title']] = {'pub_year': str(pub['bib']['pub_year']), 'num_citations': str(pub['num_citations'])}
+            if "pub_year" in pub["bib"] and str(pub["bib"]["pub_year"]) == this_year_str:
+                ret_pubs[pub["bib"]["title"]] = {"pub_year": str(pub["bib"]["pub_year"]), "num_citations": str(pub["num_citations"])}
                 cnt += 1
         return ret_pubs
 
@@ -46,24 +46,24 @@ class SearchEngine():
 class Citation():
     def __init__(self, path):
         self.path = path
-        self.today = datetime.now().strftime('%Y-%m-%d')
-        self.yesterday = (datetime.now() + timedelta(days=-1)).strftime('%Y-%m-%d')
+        self.today = datetime.now().strftime("%Y-%m-%d")
+        self.yesterday = (datetime.now() + timedelta(days=-1)).strftime("%Y-%m-%d")
         if not os.path.isfile(self.path):
             self.write({})
 
     def read(self):
-        with open(self.path, 'r', encoding='utf-8') as f:
+        with open(self.path, "r", encoding="utf-8") as f:
             d = json.load(f)
         return d
 
     def write(self, d):
-        with open(self.path, 'w', encoding='utf-8') as f:
+        with open(self.path, "w", encoding="utf-8") as f:
             json.dump(d, f)
 
     def update(self, author_id, author_name, citation):
         d = self.read()
         if author_id not in d:
-            d[author_id] = {'name':author_name}
+            d[author_id] = {"name":author_name}
         d[author_id][self.today] = citation
         self.write(d)
 
@@ -73,9 +73,9 @@ class Citation():
         try:
             yesterday_citation = d[author_id][self.yesterday]
         except Exception as e:
-            print('Execption: ', e)
+            print("Execption: ", e)
             yesterday_citation = 0
-        d[author_id]['increase'] = today_citation - yesterday_citation
+        d[author_id]["increase"] = today_citation - yesterday_citation
         self.write(d)
         
         return (today_citation, yesterday_citation)
@@ -85,9 +85,9 @@ class Citation():
         d_present = {}
         d_all = self.read()
         for author_id in d_all:
-            author_name = d_all[author_id]['name']
+            author_name = d_all[author_id]["name"]
             if author_name not in d_present:
-                d_present[author_name] = {'citation':d_all[author_id][self.today], 'increase':d_all[author_id]['increase']}
+                d_present[author_name] = {"citation":d_all[author_id][self.today], "increase":d_all[author_id]["increase"]}
         return d_present
 
     def getSequence(self, author_ids):
@@ -96,7 +96,7 @@ class Citation():
         for author_id in author_ids:
             citation_lines = []
             for key in sorted(d_all[author_id]):
-                if '-' in key and key != 'increase' and key != 'name':
+                if "-" in key and key != "increase" and key != "name":
                     # print(d_all[author_id][key])
                     citation_lines.append(int(d_all[author_id][key]))
             seq.append(citation_lines)
@@ -112,12 +112,12 @@ def getPlotData(d_all, author_ids):
     data = []
     first_ptime, earliest_date = 1, datetime(2200, 1, 1)
     for author_id in author_ids:
-        plot_data = {'X_DATA':[], 'Y_DATA':[]}
+        plot_data = {"X_DATA":[], "Y_DATA":[]}
         for key in sorted(d_all[author_id]):
-            if '-' in key and key != 'increase' and key != 'name':
-                plot_data['Y_DATA'].append(int(d_all[author_id][key]))
-                ptime = time.strptime(key, '%Y-%m-%d')
-                plot_data['X_DATA'].append(datetime(ptime[0], ptime[1], ptime[2]))
+            if "-" in key and key != "increase" and key != "name":
+                plot_data["Y_DATA"].append(int(d_all[author_id][key]))
+                ptime = time.strptime(key, "%Y-%m-%d")
+                plot_data["X_DATA"].append(datetime(ptime[0], ptime[1], ptime[2]))
                 if first_ptime == 1:
                     earliest_date = ptime
                     first_ptime = 0
@@ -126,58 +126,58 @@ def getPlotData(d_all, author_ids):
         data.append(plot_data)
     
     for i in range(len(data)):
-        for j in range(len(data[i]['X_DATA'])):
-            data[i]['X_DATA'][j] = (data[i]['X_DATA'][j] - earliest_date).days
+        for j in range(len(data[i]["X_DATA"])):
+            data[i]["X_DATA"][j] = (data[i]["X_DATA"][j] - earliest_date).days
     return (data, earliest_date)
 
 
 def checkUpdate(searcher, citation, conf, single_author=None, force=False):
     # today = time.localtime()[0:3]
-    today_str = datetime.now().strftime('%Y-%m-%d')
-    # last_modified = time.localtime(os.stat(citation.path).st_mtime)[0:3] # bug, sholdn't rely on the modify time. should fetch from json
+    today_str = datetime.now().strftime("%Y-%m-%d")
+    # last_modified = time.localtime(os.stat(citation.path).st_mtime)[0:3] # bug, sholdn"t rely on the modify time. should fetch from json
     d_all = citation.read()
-    first_author = conf.options('Authors')[0]
-    if today_str not in d_all[conf['Authors'][first_author]] or force == True:
+    first_author = conf.options("Authors")[0]
+    if today_str not in d_all[conf["Authors"][first_author]] or force == True:
     # if today != last_modified or force == True:
         if single_author is not None:
-            author_id = conf['Authors'][single_author]
-            result = searcher.search(author_id, method='id')
-            citation.update(author_id, result['name'], result['citedby'])
+            author_id = conf["Authors"][single_author]
+            result = searcher.search(author_id, method="id")
+            citation.update(author_id, result["name"], result["citedby"])
             today_citation, yesterday_citation = citation.compare(author_id)
-            print('%s today citation: %d, yesterday citation: %d, %d ⬆'%(result['name'], today_citation, yesterday_citation, (today_citation-yesterday_citation)))
+            print("%s today citation: %d, yesterday citation: %d, %d ⬆"%(result["name"], today_citation, yesterday_citation, (today_citation-yesterday_citation)))
         else:
-            for author_label in conf['Authors']:
-                author_id = conf['Authors'][author_label]
-                result = searcher.search(author_id, method='id')
-                citation.update(author_id, result['name'], result['citedby'])
+            for author_label in conf["Authors"]:
+                author_id = conf["Authors"][author_label]
+                result = searcher.search(author_id, method="id")
+                citation.update(author_id, result["name"], result["citedby"])
                 today_citation, yesterday_citation = citation.compare(author_id)
-                print('%s today citation: %d, yesterday citation: %d, %d ⬆'%(result['name'], today_citation, yesterday_citation, (today_citation-yesterday_citation)))
+                print("%s today citation: %d, yesterday citation: %d, %d ⬆"%(result["name"], today_citation, yesterday_citation, (today_citation-yesterday_citation)))
 
 
 
 def fetchLatestKPub(user_id, latest_k):
-    head = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
-    url = 'https://scholar.google.com.hk/citations?user=' + user_id + '&view_op=list_works&sortby=pubdate'
+    head = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"}
+    url = "https://scholar.google.com.hk/citations?user=" + user_id + "&view_op=list_works&sortby=pubdate"
     r = requests.get(url, headers=head)
     r.raise_for_status()
     r.encoding = r.apparent_encoding
-    soup = BeautifulSoup(r.text, 'html.parser')
-    recent_pubs = soup.find_all(class_='gsc_a_tr')
+    soup = BeautifulSoup(r.text, "html.parser")
+    recent_pubs = soup.find_all(class_="gsc_a_tr")
     k = min(20, latest_k)
 
     latest_pubs = {}
     for i in range(k):
         title = recent_pubs[i].contents[0].contents[0].string
-        pub_year = recent_pubs[i].contents[0].contents[2].contents[1].string.split(',')[-1]
+        pub_year = recent_pubs[i].contents[0].contents[2].contents[1].string.split(",")[-1]
         num_citations = recent_pubs[i].contents[1].string
         if num_citations is None:
             num_citations = 0
-        latest_pubs[title] = {'pub_year': pub_year, 'num_citations': num_citations}
+        latest_pubs[title] = {"pub_year": pub_year, "num_citations": num_citations}
     return latest_pubs
 
 
 def autoUpdateEveryDay(searcher, citation, conf):
-    print('\nAuto checkUpdate() once a day')
+    print("\nAuto checkUpdate() once a day")
     checkUpdate(searcher, citation, conf, single_author=None, force=True)
     timer = threading.Timer(86400, autoUpdateEveryDay, (searcher, citation, conf))
     timer.start()
@@ -196,18 +196,18 @@ def getSecondsToTime(hour, minute, second):
     
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     conf = ConfigParser()
-    conf.read("config.ini", encoding='utf-8')
-    Proxy = conf['Proxy']
-    Authors = conf['Authors']
-    Settings = conf['Settings']
+    conf.read("config.ini", encoding="utf-8")
+    Proxy = conf["Proxy"]
+    Authors = conf["Authors"]
+    Settings = conf["Settings"]
 
-    os.environ["http_proxy"] = Proxy['http_proxy']
-    os.environ["https_proxy"] = conf['Proxy']['https_proxy']
+    os.environ["http_proxy"] = Proxy["http_proxy"]
+    os.environ["https_proxy"] = conf["Proxy"]["https_proxy"]
 
     searcher = SearchEngine()
-    citation = Citation(Settings['db_path'])
+    citation = Citation(Settings["db_path"])
     checkUpdate(searcher, citation, conf, force=True)
     print(json.dumps(citation.read(), indent=4))
     
